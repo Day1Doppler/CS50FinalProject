1bccfbff960d2b3db04e5147450fa9d73f78c110
Day1Doppler <Day1Doppler@users.noreply.github.com>
2017-07-08 08:06:45 +0000

    20170708T080601Z

--- credit.c
+++ credit.c
@@ -41,22 +41,6 @@ bool luhn_pass(long long n)
     return sub % 10 == 0;
 }
 
-bool amex_match(long long n)
-{
-    return (num_digits(n) == 15 && (n / 10000000000000 == 34 || n / 10000000000000 == 37));
-}
-
-bool mastercard_match(long long n)
-{
-    return (num_digits(n) == 16 && (n / 100000000000000 == 51 || n / 100000000000000 == 52
-                                       || n / 100000000000000 == 53 || n / 100000000000000 == 54 || n / 100000000000000 == 55));
-}
-
-bool visa_match(long long n)
-{
-    return ((num_digits(n) == 13 || num_digits(n) == 16) && (n / 1000000000000000 == 4 || n / 1000000000000 == 4));
-}
-
 string card_type(long long n)
 {
     if (amex_match(n)) {
@@ -86,4 +70,20 @@ int sum_digits(int n)
 int num_digits(long long n)
 {
     return ceil(log10(n));
+}
+
+bool amex_match(long long n)
+{
+    return (num_digits(n) == 15 && (n / 10000000000000 == 34 || n / 10000000000000 == 37));
+}
+
+bool mastercard_match(long long n)
+{
+    return (num_digits(n) == 16 && (n / 100000000000000 == 51 || n / 100000000000000 == 52
+                                       || n / 100000000000000 == 53 || n / 100000000000000 == 54 || n / 100000000000000 == 55));
+}
+
+bool visa_match(long long n)
+{
+    return ((num_digits(n) == 13 || num_digits(n) == 16) && (n / 1000000000000000 == 4 || n / 1000000000000 == 4));
 }
